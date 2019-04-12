1c62086d4c421ec84c926c15ef4b1b7053cdada9
Rachel Mc Grane Maher <c15529443@BUS-3079-08.ict.ad.dit.ie>
2019-04-12 15:19:46 +0100

    Empty Calculator class created

--- src/calculator1/Calculator.java
+++ src/calculator1/Calculator.java
@@ -1,5 +1,29 @@
+/*
+  *Rachel Mc Grane Maher
+   *12th April
+    *This is an application of a calculator that can add,subtract,multiply and divide
+    *
+*/
 package calculator1;
 
-public class Calculator {
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+
+public class Calculator extends JFrame {
+
+	
 
 }
